d70dcb876fd737e400483678b69a0cd9f7281ab6
Praveen Kumar Pendyala <praveendath92@gmail.com>
2013-11-09 01:20:38 +0530

    Routing of 25 LUTs complete
    Routing done in 3 horizontal rows.

--- pa.fromNetlist.tcl
+++ pa.fromNetlist.tcl
@@ -1,10 +1,10 @@
 
 # PlanAhead Launch Script for Post-Synthesis pin planning, created by Project Navigator
 
-create_project -name LUTs -dir "E:/Dropbox/Works and collections/Elec/Xilinx_learning/LUTs/planAhead_run_1" -part xc5vlx110tff1136-1
+create_project -name LUTs -dir "E:/Dropbox/Works and collections/Elec/Xilinx_learning/PDL/planAhead_run_1" -part xc5vlx110tff1136-1
 set_property design_mode GateLvl [get_property srcset [current_run -impl]]
-set_property edif_top_file "E:/Dropbox/Works and collections/Elec/Xilinx_learning/LUTs/main.ngc" [ get_property srcset [ current_run ] ]
-add_files -norecurse { {E:/Dropbox/Works and collections/Elec/Xilinx_learning/LUTs} }
+set_property edif_top_file "E:/Dropbox/Works and collections/Elec/Xilinx_learning/PDL/main.ngc" [ get_property srcset [ current_run ] ]
+add_files -norecurse { {E:/Dropbox/Works and collections/Elec/Xilinx_learning/PDL} }
 set_param project.pinAheadLayout  yes
 set_property target_constrs_file "constraints.ucf" [current_fileset -constrset]
 add_files [list {constraints.ucf}] -fileset [get_property constrset [current_run]]
